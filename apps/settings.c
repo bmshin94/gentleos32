ed16a2063532e42e23f5ae7317fbdd206f6631e1
luke8086 <55237178+luke8086@users.noreply.github.com>
2026-09-13 21:14:55 +0200

    Settings: Remove auto adjustment of desktop color 1

--- apps/settings.c
+++ apps/settings.c
@@ -270,18 +270,6 @@ set_wallpaper(bitmap_st *bitmap)
 
     gui_wm_wallpaper = bitmap;
 
-    /* Pixel art usually works best with black grid */
-    if (bitmap && gui_wm_is_valid_pixelart(bitmap)) {
-        set_desktop_color1(0);
-    }
-
-    /* Patterns usually work best with colors from the current theme */
-    if (bitmap && gui_wm_is_valid_pattern(bitmap)) {
-        gui_theme_set(gui_theme.index);
-        set_desktop_color1(gui_theme.desktop);
-        set_desktop_color2(gui_theme.desktop_alt);
-    }
-
     select_active_pattern_button();
     select_active_wallpaper_item();
 
